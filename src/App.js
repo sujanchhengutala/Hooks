90c36e7afb2950b7da494b08bb94a5a2f5d7de24
sujanchhengutala <sujanchhengutala9@gmail.com>
2022-09-28 10:01:30 +0545

    Hooks

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import UseState from "./component/Hooks/UseState";
+
+function App() {
+  return (
+<>
+<UseState />
+</>
+  );
+}
+
+export default App;
---
+++ src/component/Hooks/UseState.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './style.css'
+
+const UseState = () => {
+  return (
+    <>
+    <div className="center_div">
+        <p>My Data</p>
+        <div class="button2">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Increment
+        </div>
+        <div class="button2">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Decrement
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default UseState

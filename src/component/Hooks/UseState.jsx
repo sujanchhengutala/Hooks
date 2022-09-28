de67a8be7941601d36a4517b1430aed0cf0d7269
sujanchhengutala <sujanchhengutala9@gmail.com>
2022-09-28 10:28:30 +0545

    increment-decrement using useState

--- src/component/Hooks/UseState.jsx
+++ src/component/Hooks/UseState.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './style.css'
 
+
 const UseState = () => {
+    const initialState = 0;
+    const [data, setData] = useState(initialState)
+    const increment = ()=>{
+        
+        setData(data+1)
+    }
+    const decrement = ()=>{
+        data > 0 ?setData(data-1):setData(0);
+        
+    }
   return (
     <>
     <div className="center_div">
-        <p>My Data</p>
-        <div class="button2">
+        <p>{data}</p>
+        <div class="button2" onClick={()=>increment()}>
             <span></span>
             <span></span>
             <span></span>
             <span></span>
             Increment
         </div>
-        <div class="button2">
+        <div class="button2" onClick={()=>decrement()}>
             <span></span>
             <span></span>
             <span></span>
